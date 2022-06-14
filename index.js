5164f7d5b1d3e1c6dd2eb4ac52e039435fcc8034
Will <swilson.carlson@hotmail.com>
2022-06-14 13:49:36 -0400

    commit tree 7be6210561768bc875b13a4be5e32437f5d93a05

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: "James",
+    streetAddress: "12 Buckingham Esplanade",
+  };
+
+  function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+  
+    newObj[key] = value;
+  
+    return newObj;
+  };
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+  
+    return obj;
+  }
+
+  function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj};
+    console.log("newObj",newObj);
+    delete newObj[key];
+  
+    return newObj;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+  
+    return obj;
+  }
+
+  
